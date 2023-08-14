132956a7542f2d5340beeafed93bf9643a2f5e00
gabrielgalvaodc <129107924+gabrielgalvaodc@users.noreply.github.com>
2023-08-14 13:47:52 -0300

    codigo inicial

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+$con = mysqli_connect("localhost","root","","crud-php-bootstrap");
+if(!$con){
+  die('connection failed'. mysqli_connect_error());
+}
+?>
---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+require 'dbcon.php';
+?>
+<!doctype html>
+<html lang="pr-BR">
+<head>
+  <!--Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"rel="stylesheet">
+
+  <title>CRUD PHP</title>
+  
+</head>
+<body>
+  <div class="container mt-4">
+    <?php include('message.php'); ?>
+   <div class="row">
+     <div class="cold-md-12">
+       <div class="card">
+         <div class="card-hearder">
+           <h4>Detalhes do aluno
+           <a href="students-create.php" clas="btn btn-primary float-end">Adicionar Aluno</a>
+           </h4>
+         </div>
+         <div class="card-body">
+           <table class="table table-berdered table-striped">
+             <thead>
+               <tr>
+                   <th>ID</th>
+                   <th>Nome</th>
+                   <th>Email</th>
+                   <th>Telefone</th>
+                   <th>Curso</th>
+                   <th>Ação</th>
+               </tr>
+             </thead>
+             <tbody>
+               <?php
+                   $query = "SELECT * FROM students";
+                   $query_run = mysqly_query($con, $query);
+
+                if(mysqli_num_rows($query_run) > 0)
+                {
+                  foreach($query_run as $student)
+                    {
+                      ?>
+                      <tr>
+                        <td><?= $student['id']; ?></td>
+                        <td><?= $student['name']; ?></td>
+                         <td><?= $student['email']; ?></td>
+                         <td><?= $student['phone']; ?></td>
+                         <td><?= $student['course']; ?></td>
+                        <td>
+                          <a href="student-view.php?id=<?=$student['id']; ?>" class="btn btn-info btn-sm">Visualizar</a>
+                          <a href="students-edit.php?id=<?=$student["id"]; class="btn btn-success btn-sm">Editar</a>
+                        <form action="code.php" method="POST"
+                        class="d-inline">
+                        <button type="submit"
+                        name="delete_student" value="<?=$student['id'];?>" class="btn btn-danger
+                        btn-sm">Deletar</button>
+                        </form>
+                        </td>
+                      </tr>
+                        <?php
+                    }
+                }
+                else
+                {
+                  echo "<h5> nenhum aluno cadastrado </h5>";
+                }
+              ?>
+             </tbody>
+                            </table>
+                                 </div>
+                               </div>
+                              </div>
+                             </div>
+                           </div>
+
+                            <script
+                            src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></scri
+pt>       
+</body>
+  </html>
